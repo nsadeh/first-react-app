f82fb3d1afdc8bcaf4301f2a9cc6a03d94c0eae0
Nimrod Sadeh <nsadeh@users.noreply.github.com>
2021-08-21 17:10:37 -0400

    basic layout of profiles page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 import React, { FunctionComponent } from "react";
-import "./styles.scss";
 import "semantic-ui-css/semantic.min.css";
 import {
   AppBar,
   BottomNavigation,
   BottomNavigationAction,
-  Toolbar
+  Card,
+  CardContent,
+  Toolbar,
+  Typography
 } from "@material-ui/core";
 import RestoreIcon from "@material-ui/icons/Restore";
+
+const ProfileCard: React.FunctionComponent<IProfileProps> = (props) => {
+  return (
+    <Card className="person-card">
+      <div>
+        <CardContent className="person-card-con">
+          <Typography component="h5">{props.message}</Typography>
+        </CardContent>
+      </div>
+    </Card>
+  );
+};
+
+type IProfileListProps = {
+  profiles: Array<IProfileProps>;
+};
+
+type IProfileProps = {
+  message: string;
+};
+
+const Profiles: React.FunctionComponent<IProfileListProps> = (props) => {
+  return (
+    <div className="profile-card-list">
+      {props.profiles.map((p) => (
+        <div>{ProfileCard(p)}</div>
+      ))}
+    </div>
+  );
+};
+
+const profiles: Array<IProfileProps> = [
+  { message: "I am a profile" },
+  { message: "I, too, am a profile" }
+];
 
 const App: FunctionComponent = () => {
   return (
@@ -15,7 +52,8 @@ const App: FunctionComponent = () => {
       <AppBar position="static">
         <Toolbar></Toolbar>
       </AppBar>
-      <BottomNavigation>
+      <Profiles profiles={profiles} />
+      <BottomNavigation className="bottom-navbar">
         <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
       </BottomNavigation>
     </div>
